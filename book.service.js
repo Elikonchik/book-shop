6354af3e606d995dcb02988dfca0cfe3998d7d6d
Elik <elikbelikshluk@gmail.com>
2026-08-21 15:41:32 +0300

    Add initial implementation of book shop application with controller, service, and UI

---
+++ book.service.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const gBooks = [{
+    id: 'bg4J78',
+    title: 'The adventures of Lori Ipsi',
+    price: 120,
+    imgUrl: 'lori-ipsi.jpg'
+},
+{
+    id: 'bg4J79',
+    title: 'World atlas',
+    price: 300,
+    imgUrl: 'World atlas.jpg'
+},
+{
+    id: 'bg4J80',
+    title: 'Zorba the greek',
+    price: 87,
+    imgUrl: 'Zorba the greek.jpg'
+}]
+
+'use strict'
+
+function getBooks() {
+    return gBooks
+}
