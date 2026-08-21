2feb3302440e34929fc5a2f02ab9aa4f1babaf16
Elik <elikbelikshluk@gmail.com>
2026-08-21 16:12:28 +0300

    Implement the render() function&Remove the hard coded table

--- book.controller.js
+++ book.controller.js
@@ -1,9 +1,33 @@
 'use strict'
 
 function onInit() {
-    onRender()
+    render()
 }
 
-function onRender() {
+function render() {
 
+    const books = getBooks()
+
+    const elTbody = document.querySelector('tbody')
+
+    let strHTML = ''
+
+    books.forEach(function (book) {
+
+        strHTML += `
+            <tr>
+                <td>${book.title}</td>
+                <td>${book.price}</td>
+                <td>
+                    <button class="read">Read</button>
+                    <button class="update">Update</button>
+                    <button class="delete">Delete</button>
+                </td>
+            </tr>
+        `
+    })
+
+    elTbody.innerHTML = strHTML
 }
+
+onInit()
